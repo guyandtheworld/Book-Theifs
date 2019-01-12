068b862d2e27d846e3039bef48c1c21c0a365ce8
pranavmodx <pranavmodx@gmail.com>
2019-01-12 13:46:25 +0530

    added code to fetch book title

--- main.py
+++ main.py
@@ -5,16 +5,19 @@ from bs4 import BeautifulSoup
 
 def acrawler():
 
-    isbn = "0345453743"
-    url = "https://www.amazon.in/dp/" + isbn
-    print("URL= " , url)
-    # while loop here
-    source = requests.get(url, "html.parsar")
-    source_code = source.text
-    soup = BeautifulSoup(source_code)
-
-    for link in soup.find_all('a'):
-        print(link.get('href'))
+    isbn = ["0345453743","1847370292"]
+    for i in range(len(isbn)):
+        url = f"https://www.amazon.in/dp/{isbn[i]}"
+        print("URL = " , url)
+        # while loop here
+        source = requests.get(url)
+        source_code = source.text
+        soup = BeautifulSoup(source_code, "lxml")
+        section = soup.find('div', id='booksTitle')
+        title = section.span.text
+        print(title)
+        # for link in soup.find_all('a'):
+        #         print(link.get('href'))
 
 
 acrawler()
