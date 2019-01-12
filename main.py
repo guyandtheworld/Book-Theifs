9b78b8e87a5df822a6093c13d38c9690707bc6c5
pranavmodx <pranavmodx@gmail.com>
2019-01-12 17:03:54 +0530

    product details

--- main.py
+++ main.py
@@ -1,23 +1,31 @@
 import requests
 from bs4 import BeautifulSoup
-
+import time
 
 
 def acrawler():
 
-    isbn = ["0345453743","1847370292"]
+    isbn = ["0345453743","1547291817"]
+
     for i in range(len(isbn)):
+
         url = f"https://www.amazon.in/dp/{isbn[i]}"
         print("URL = " , url)
-        # while loop here
         source = requests.get(url)
         source_code = source.text
         soup = BeautifulSoup(source_code, "lxml")
-        section = soup.find('div', id='booksTitle')
-        title = section.span.text
-        print(title)
-        # for link in soup.find_all('a'):
-        #         print(link.get('href'))
 
+        book_title = soup.find('div', id='booksTitle')
+        title = book_title.span.text
+        print(f"Book title : {title}")
+
+        pd_details = soup.find('td', class_="bucket")
+        elem = pd_details.div.ul
+
+        print('Product Details')
+        for li in elem.find_all('li')[0:5]:
+            print(li.text)
+
+        time.sleep(20) # For reducing traffic
 
 acrawler()
