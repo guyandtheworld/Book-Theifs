2b7305538b9027dc47a24df22e1e23f2df076d9f
Sandeep Pillai <sandeep.pillai42@gmail.com>
2019-01-12 16:28:02 +0530

    Update flipkart.py

--- flipkart.py
+++ flipkart.py
@@ -1,29 +1,50 @@
 import requests
 from bs4 import BeautifulSoup
 
+def is_valid_isbn(isbn):
+
+    sum = 0
+    if len(isbn) == 13:
+
+        for i in range(13):
+            if(i % 2 == 0):
+                sum = sum + int(isbn[i])  # asuming this is 0..9, not '0'..'9'
+
+            else:
+                sum = sum + int(isbn[i]) * 3
+
+        if sum % 10 == 0:
+            return True
 
 
-def fcrawler():
+    return False
 
-    isbn = ["9781582701707"]
-    for i in range(len(isbn)):
-        url = "https://www.flipkart.com/placeholder/p/itmdytkfgwnuz7gt?pid=" + isbn[i]
-        print("URL = " , url)
-        # while loop here
+print(is_valid_isbn("9780000000000"))
+isbnum = 9788183225090
+
+
+
+def fcrawler(url):
+
+        print("URL = ", url)
+
         source = requests.get(url)
-        source_code = source.text
+        source_code = source.text                           # BS4 stuff, ignore.
         soup = BeautifulSoup(source_code, "lxml")
-        
+
+
         for item in soup.find_all("title"):
-            print("Name:: " , item.string)
-            
-        for item in soup.find_all("li", {"class": "_2-riNZ"}):
-            print(item.string)
+            if(item.string!="Buy Products Online at Best Price in India - All Categories | Flipkart.com"):
+                print("Name:: ", item.string)
+                for book in soup.find_all("li", {"class": "_2-riNZ"}):
+                    print(book.string)
+        print('\n'*5)
+
+def main():
+    for i in range(100):
+        if is_valid_isbn(str(isbnum + i)):
+            url = "https://www.flipkart.com/placeholder/p/itmdytkfgwnuz7gt?pid=" + str(isbnum+i)
+            fcrawler(url)
 
 
-
-
-
-
-
-fcrawler()
+main()
